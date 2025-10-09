62d477e57dd2213c11a30d6cf1295a465f536fbc
Cambridge-IoE-Group <hc663@cam.ac.uk>
2025-10-09 13:56:07 +0000

    Deploying to gh-pages from @ Cambridge-IoE-Group/Cambridge-IoE-Group.github.io@174f87ac6826aae8cef5de7ccb435e44200fbfd3 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -489,13 +489,6 @@ ninja.data = [{
           window.open("https://github.com/Cambridge-IoE-Group", "_blank");
         },
       },{
-        id: 'social-linkedin',
-        title: 'LinkedIn',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.linkedin.com/in/özgür-barış-akan-90047a72", "_blank");
-        },
-      },{
         id: 'social-scholar',
         title: 'Google Scholar',
         section: 'Socials',
