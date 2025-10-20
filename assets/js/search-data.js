46abeccacfd894c8cd89fa54664a6b61a60ef292
GuangLun2000 <HANLIN.CAI.2021@MUMAIL.IE>
2025-10-20 14:48:37 +0000

    Deploying to gh-pages from @ Cambridge-IoE-Group/Cambridge-IoE-Group.github.io@acede2311959dff2fcbacc10dd79b06bd565d230 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -405,19 +405,19 @@ ninja.data = [{
           description: "",
           section: "News",handler: () => {
               window.location.href = "/news/announcement_3/";
-            },},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+            },},{id: "projects-axa-research-fund-2020-2025",
+          title: 'AXA RESEARCH FUND (2020-2025)',
+          description: "AXA Chair in Molecular Information and Communication Technologies",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-european-research-council-erc-2014-2019",
+          title: 'European Research Council (ERC, 2014-2019)',
+          description: "MINERVA:Communication Theoretical Foundations of Nervous System Towards Bio-Inspired Nanonetworks and ICT-Inspired Neuro-Treatment",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-tubitak-1003-2014-2017",
+          title: 'TUBITAK 1003 (2014-2017)',
+          description: "TeraFemto:Terahertz Femtocell for 5G Mobile Networks",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
             },},{id: "projects-project-4",
