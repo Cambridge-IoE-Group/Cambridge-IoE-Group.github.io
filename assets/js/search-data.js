ce5f5702947b78049788fc134dc2ea4a546120ac
GuangLun2000 <HANLIN.CAI.2021@MUMAIL.IE>
2025-10-21 19:15:02 +0000

    Deploying to gh-pages from @ Cambridge-IoE-Group/Cambridge-IoE-Group.github.io@ab2074479a48b06c9a658eb7554fa91713cfc85d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -39,7 +39,7 @@ ninja.data = [{
           },
         },{id: "nav-projects",
           title: "projects",
-          description: "Projects of the IoE Group",
+          description: "Our research activities form the foundation for the projects we undertake. By exploring fundamental principles in molecular communications, nano-networks, and emerging paradigms such as the Internet of Bio-Nano Things, we establish the theoretical and experimental groundwork that drives our applied initiatives. These projects not only test and validate our models in real-world scenarios but also create new insights that feed back into our ongoing research.The projects below represent concrete extensions of our research directions. Each one transforms core ideas into interdisciplinary applications, engaging with partners across academia, industry, and society to demonstrate the impact of molecular communications and beyond.",
           section: "Navigation",
           handler: () => {
             window.location.href = "/projects/";
@@ -485,8 +485,8 @@ ninja.data = [{
           description: "Green Visible Light Communications (2012-2013)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-intel",
-          title: 'INTEL',
+            },},{id: "projects-intel-corporation",
+          title: 'Intel Corporation',
           description: "Energy Efficient Scheduling for LTE (2012-2013)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
