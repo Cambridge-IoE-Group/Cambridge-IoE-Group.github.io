ae6a685993a58fc19df8be8520174d7479289d7d
GuangLun2000 <HANLIN.CAI.2021@MUMAIL.IE>
2025-10-21 18:40:19 +0000

    Deploying to gh-pages from @ Cambridge-IoE-Group/Cambridge-IoE-Group.github.io@5fe1e378939d1302a1c4c7f90edbe1119e8fd4f4 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -422,12 +422,12 @@ ninja.data = [{
               window.location.href = "/projects/3_project/";
             },},{id: "projects-european-commission",
           title: 'European Commission',
-          description: "CIRCLE: Coordinating European Research on Molecular Communications",
+          description: "CIRCLE: Coordinating European Research on Molecular Communications (2015-2017)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
             },},{id: "projects-lockheed-martin",
           title: 'Lockheed Martin',
-          description: "Mobile Ad Hoc beyond-Line-of-Sight (b-LoS) Networks",
+          description: "Mobile Ad Hoc beyond-Line-of-Sight (b-LoS) Networks (2012-2015)",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
             },},{id: "projects-project-6",
